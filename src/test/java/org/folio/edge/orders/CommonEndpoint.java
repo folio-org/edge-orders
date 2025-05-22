5b37357039f3c8712af8098ba4d8a6a64ce03107
Boburbek Kadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-05-22 18:31:51 +0500

    [MODMO-15]. Update MosaicOrder schema & add more endpoints to edge-orders (#95)
    * [MODMO-15]. Update MosaicOrder schema & add more endpoints to edge-orders
    
    * [MODMO-15]. Add type==staff extra query to GET /users
    
    * [MODMO-15]. Remove type==staff on GET /users
    
    * [MODMO-15]. Update new routes with a suffix
    
    * [MODMO-15]. Apply review recommendations
    
    * [MODMO-15]. Update schema
    
    * [MODMO-15]. Rename constructor param & member field

--- src/test/java/org/folio/edge/orders/CommonEndpoint.java
+++ src/test/java/org/folio/edge/orders/CommonEndpoint.java
@@ -8,18 +8,21 @@ public enum CommonEndpoint {
   ACQUISITIONS_UNITS("/acquisitions-units", "/acquisitions-units/units", "acquisitionsUnits", true),
   ACQUISITIONS_METHODS("/acquisition-methods", "/orders/acquisition-methods", "acquisitionMethods", true),
   ORGANIZATIONS("/organizations", "/organizations/organizations", "organizations", true),
-  BILLING_AND_SHIPPING("/addresses/billing-and-shipping", "/configurations/entries", "configs", false);
+  BILLING_AND_SHIPPING("/addresses/billing-and-shipping", "/configurations/entries", "configs", false),
+  LOCATIONS("/locations-for-order", "/locations", "locations", true),
+  MATERIAL_TYPES("/material-types-for-order", "/material-types", "mtypes", true),
+  USERS("/users-for-order", "/users", "users", true);
 
   private final String ingressUrl;
   private final String egressUrl;
   private final String dataKey;
-  private final boolean hasFiltering;
+  private final boolean isEmptyExtraQuery;
 
-  CommonEndpoint(String ingressUrl, String egressUrl, String dataKey, boolean hasFiltering) {
+  CommonEndpoint(String ingressUrl, String egressUrl, String dataKey, boolean isEmptyExtraQuery) {
     this.ingressUrl = ingressUrl;
     this.egressUrl = egressUrl;
     this.dataKey = dataKey;
-    this.hasFiltering = hasFiltering;
+    this.isEmptyExtraQuery = isEmptyExtraQuery;
   }
 
   public String getIngressUrl() {
@@ -34,7 +37,7 @@ public enum CommonEndpoint {
     return dataKey;
   }
 
-  public boolean isHasFiltering() {
-    return hasFiltering;
+  public boolean isEmptyExtraQuery() {
+    return isEmptyExtraQuery;
   }
 }
