18cce7bb6bb6e7af83456d04459ee8d4e9dee53e
Boburbek Kadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-06-05 19:31:37 +0500

    [EDGORDERS-101]. Enhancements for Mosaic integration support (#97)
    * [EDGORDERS-101]. Enhancements for Mosaic integration support
    
    * [EDGORDERS-101]. Rename a constant

--- src/test/java/org/folio/edge/orders/CommonEndpoint.java
+++ src/test/java/org/folio/edge/orders/CommonEndpoint.java
@@ -1,27 +1,32 @@
 package org.folio.edge.orders;
 
 public enum CommonEndpoint {
-  ORDER_TEMPLATES("/orders/order-templates", "/orders/order-templates", "orderTemplates", true),
-  ORDER_CUSTOM_FIELDS("/orders/custom-fields", "/custom-fields", "customFields", true),
-  FUNDS("/finance/funds", "/finance/funds", "funds", true),
-  EXPENSE_CLASSES("/finance/expense-classes", "/finance/expense-classes", "expenseClasses", true),
-  ACQUISITIONS_UNITS("/acquisitions-units", "/acquisitions-units/units", "acquisitionsUnits", true),
-  ACQUISITIONS_METHODS("/acquisition-methods", "/orders/acquisition-methods", "acquisitionMethods", true),
-  ORGANIZATIONS("/organizations", "/organizations/organizations", "organizations", true),
-  BILLING_AND_SHIPPING("/addresses/billing-and-shipping", "/configurations/entries", "configs", false),
-  LOCATIONS("/locations-for-order", "/locations", "locations", true),
-  MATERIAL_TYPES("/material-types-for-order", "/material-types", "mtypes", true),
-  USERS("/users-for-order", "/users", "users", true);
+  ORDER_TEMPLATES("/orders/order-templates", "/orders/order-templates", "orderTemplates", true, true),
+  ORDER_CUSTOM_FIELDS("/orders/custom-fields", "/custom-fields", "customFields", true, true),
+  FUNDS("/finance/funds", "/finance/funds", "funds", true, true),
+  EXPENSE_CLASSES("/finance/expense-classes", "/finance/expense-classes", "expenseClasses", true, true),
+  FUND_CODES_EXPENSE_CLASSES("/finance/fund-codes-expense-classes", "/finance/fund-codes-expense-classes", "fundCodeVsExpClassesTypes", false, true),
+  ACQUISITIONS_UNITS("/acquisitions-units", "/acquisitions-units/units", "acquisitionsUnits", true, true),
+  ACQUISITIONS_METHODS("/acquisition-methods", "/orders/acquisition-methods", "acquisitionMethods", true, true),
+  ORGANIZATIONS("/organizations", "/organizations/organizations", "organizations", true, true),
+  BILLING_AND_SHIPPING("/addresses/billing-and-shipping", "/configurations/entries", "configs", true, false),
+  LOCATIONS("/locations-for-order", "/locations", "locations", true, true),
+  MATERIAL_TYPES("/material-types-for-order", "/material-types", "mtypes", true, true),
+  IDENTIFIER_TYPES("/identifier-types-for-order", "/identifier-types", "identifierTypes", true, true),
+  CONTRIBUTOR_NAME_TYPES("/contributor-name-types-for-order", "/contributor-name-types", "contributorNameTypes", true, true),
+  USERS("/users-for-order", "/users", "users", true, true);
 
   private final String ingressUrl;
   private final String egressUrl;
   private final String dataKey;
+  private final boolean isAdvancedQuerySupported;
   private final boolean isEmptyExtraQuery;
 
-  CommonEndpoint(String ingressUrl, String egressUrl, String dataKey, boolean isEmptyExtraQuery) {
+  CommonEndpoint(String ingressUrl, String egressUrl, String dataKey, boolean isAdvancedQuerySupported, boolean isEmptyExtraQuery) {
     this.ingressUrl = ingressUrl;
     this.egressUrl = egressUrl;
     this.dataKey = dataKey;
+    this.isAdvancedQuerySupported = isAdvancedQuerySupported;
     this.isEmptyExtraQuery = isEmptyExtraQuery;
   }
 
@@ -35,6 +40,10 @@ public enum CommonEndpoint {
 
   public String getDataKey() {
     return dataKey;
+  }
+
+  public boolean isAdvancedQuerySupported() {
+    return isAdvancedQuerySupported;
   }
 
   public boolean isEmptyExtraQuery() {
